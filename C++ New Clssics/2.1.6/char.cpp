365c323a4bbd7aa7e6765106c48d0a046929c02c
zeki <zeki_you@foxmail.com>
2022-10-19 22:30:12 +0800

    feat: add 2.1.6 demo

---
+++ C++ New Clssics/2.1.6/char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    //97
+    char c = '\141';
+    cout << c << endl;
+
+    //97
+    c = '\x61';
+    cout << c << endl;
+
+    return 0;
+}
